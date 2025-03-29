9c6c3d15b5e741cd860f8ba7db42e39bd2426af4
totdking <125102529+totdking@users.noreply.github.com>
2025-03-29 17:54:01 +0100

    Update sol_private.js

--- sol_private.js
+++ sol_private.js
@@ -1,6 +1,12 @@
-const { Keypair } = require('@solana/web3.js');
-
-const keypair = Keypair.fromSecretKey(Uint8Array.from(d));
-console.log("Solana Private Key:", Buffer.from(keypair.secretKey).toString('hex'));
-console.log("Solana Public Key:", keypair.publicKey.toBase58());
+const { Keypair ,PublicKey} = require('@solana/web3.js');
+const d = []; //this is your byte array
+  
+  // Convert to hex private key
+  const privateKeyHex = d.map(byte => byte.toString(16).padStart(2, '0')).join('');
+  const privateKey = Keypair.fromSecretKey(Uint8Array.from(d));  
+  const publicKey = privateKey.publicKey.toBase58();
+  console.log("Private Key (Hex):", privateKeyHex);
+  console.log("Public key: ", publicKey );
+  
 //node run filename.js
+//
